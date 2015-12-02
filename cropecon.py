cd1099b2e2b1d443d0eb82c71972c527b75dda4e
unknown <kycalica@gmail.com>
2015-12-02 15:54:27 -0800

    added requirements and install cmd file

--- cropecon.py
+++ cropecon.py
@@ -9,3 +9,27 @@ import xlrd, xlwt
 import xlutils 
 import csv
 
+#opens the workbook
+print("Opening Workbook")
+wb = xlrd.open_workbook("ag_hr_1998")
+
+#opens the worksheet: can do this by index or name 
+print("Opening Worksheets")
+icahr = wb.sheet_by_name('ICA HR') # might need to escape space
+etaw = wb.sheet_by_name('ETAW HR')
+
+#data cell's values are accessible by sheet.cell(i,j).value
+print(icahr.cell(0,0).value) 
+print(etaw.cell(0,0).value) 
+
+
+
+
+
+
+
+"""
+References: 
+http://www.sitepoint.com/using-python-parse-spreadsheet-data/ 
+
+"""
